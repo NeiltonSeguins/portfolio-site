2d9cb21d226af9ed4339a5096b915cc01f869283
NeiltonSeguins <seguins.neilton@gmail.com>
2025-06-09 12:23:38 -0300

    refactor: refatora o ItemCard para um componente novo

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-import Link from "../Link";
 import { CardItem } from "@/@types/schema";
+import ItemCard from "../ItemCard";
 
 type SectionProps = {
   title: string;
@@ -16,27 +16,7 @@ const Section = ({ title, items }: SectionProps) => {
         {/* Container responsivo */}
         <div className="flex gap-6 overflow-x-auto scrollbar-hidden snap-x snap-mandatory">
           {items.map((item) => (
-            <div
-              key={item.id}
-              className="flex-shrink-0 w-64 sm:w-72 border rounded-lg p-4 flex flex-col justify-between hover:shadow-lg snap-center"
-            >
-              {item.cover && (
-                <img
-                  src={item.cover}
-                  alt={item.title}
-                  className="w-16 sm:w-20 h-16 sm:h-20 rounded-full mb-4 border"
-                />
-              )}
-              <h3 className="text-base sm:text-lg font-semibold mb-2">
-                {item.title}
-              </h3>
-              <p className="text-sm mb-4">{item.description}</p>
-              {item.link && (
-                <Link as="router" href={item.link}>
-                  Saiba mais →
-                </Link>
-              )}
-            </div>
+            <ItemCard key={item.id} item={item} />
           ))}
         </div>
       </div>
