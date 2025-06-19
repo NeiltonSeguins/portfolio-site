c2f731e2033e59570c189304807a53c01b6174cc
NeiltonSeguins <seguins.neilton@gmail.com>
2025-06-19 14:04:24 -0300

    fix: adiciona o pacote @vercel/analytics e integra ao layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -35,7 +36,10 @@ export default function RootLayout({
           <div className="flex justify-center min-h-screen">
             <div className="w-full max-w-[1000px] px-4 flex flex-col min-h-screen">
               <Header />
-              <main className="flex-1">{children}</main>
+              <main className="flex-1">
+                {children} 
+                <Analytics />
+              </main>
               <Footer />
             </div>
           </div>
